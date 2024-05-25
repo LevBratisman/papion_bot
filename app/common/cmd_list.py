e4a4d9f0d59d28827f489702a10830fd962c96cf
Алекс Тырданов <alekstyrdanov@MacBook-Pro-2.local>
2024-05-25 13:17:17 +0300

    adding docs

--- app/common/cmd_list.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import BotCommand
-
-private = [
-    BotCommand(command="start", description="Запустить бота"),
-    BotCommand(command="menu", description="Перейти в меню"),
-    BotCommand(command="about", description="Информация о боте / Инструкции"),
-    BotCommand(command="feedback", description="Оставить отзыв"),
-    BotCommand(command="contacts", description="Контакты"),
-]
